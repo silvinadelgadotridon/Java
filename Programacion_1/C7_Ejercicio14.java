e973ea8e56072b9797006072ca1e33bb830bf482
silvinadelgadotridon <113477213+silvinadelgadotridon@users.noreply.github.com>
2022-12-30 14:01:38 -0300

    Update C7_Ejercicio14.java

--- Programacion_1/C7_Ejercicio14.java
+++ Programacion_1/C7_Ejercicio14.java
@@ -1,5 +1,3 @@
-## Hacer un programa que dado el arreglo definido y precargado, y un número entero ingresado por el usuario, copie de forma continua las secuencias de tamaño igual al número ingresado en otro arreglo de iguales características e inicializado con 0. La copia en este último arreglo deben comenzar desde el principio del mismo.
-
 import java.util.Random;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
